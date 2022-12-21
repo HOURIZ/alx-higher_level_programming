5c498f8fe7bf058edac2ab9e6f039efa0521c50a
HOURIZ <musanayusuf95@gmail.com>
2022-12-20 18:57:20 -0800

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-console.log(list);
-console.log(list.map((x, i) => x * i));
+// script that imports an array and computes a new array
+
+const array = require('./100-data').list;
+
+console.log(array);
+let cont = 0;
+const map1 = array.map(function (x) {
+  return (x * cont++);
+});
+console.log(map1);
